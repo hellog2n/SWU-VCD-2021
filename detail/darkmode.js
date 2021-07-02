a657be3df02abf46a5c01d6aa9b7cd118eed1912
hellog2n <wkddbjyjmail@gmail.com>
2021-07-02 19:14:56 +0900

    [UPDATE] detail.html 흑백 모드 추가

--- detail/darkmode.js
+++ detail/darkmode.js
@@ -1,40 +1,35 @@
 // Javascript
 
 
-
+let darkSetting = true;
 
 // main.html에 맞게 되어있음. work.html에도 에러 없이 적용이 필요
 function settingDarkWhite(value){
-    const Plists = document.getElementsByTagName("p");
+    const DivLists = document.getElementsByTagName("div");
     const Alists = document.getElementsByTagName("a");
-    const fileName = document.location.href.split('/')[(document.location.href.split('/').length-1)].split('.')[0];
-    console.log(fileName);
-
+    const Wrapper = document.getElementsByClassName("chat-wrapper")[0];
+    
     switch(value){
         
-        case 'dark':
-            document.body.style.backgroundColor = "black";
+        case true:
+        document.body.style.backgroundColor = "black";
+        Wrapper.style.backgroundColor = "black";
         document.body.style.color = "white";
-        if(fileName === 'main'){
-        document.getElementsByClassName("navbar")[0].style.backgroundColor = "#11111188";
-        document.getElementsByClassName("navbar")[0].style.color = "white";
-        }
-    for(let idx = 0; idx <Plists.length; idx++){
-        Plists[idx].style.color = "white";
+    for(let idx = 0; idx <DivLists.length; idx++){
+        DivLists[idx].style.color = "white";
     }
     for(let idx = 0; idx <Alists.length; idx++){
         Alists[idx].style.color = "white";
     }
+    
     break;
-    case 'light':
+    case false:
         document.body.style.backgroundColor = "white";
+        Wrapper.style.backgroundColor = "white";
         document.body.style.color = "black";
-        if(fileName === 'main'){
-        document.getElementsByClassName("navbar")[0].style.backgroundColor = "#ffffff88";
-        document.getElementsByClassName("navbar")[0].style.color = "black";
-    }
-    for(let idx = 0; idx <Plists.length; idx++){
-        Plists[idx].style.color = "black";
+        
+    for(let idx = 0; idx <DivLists.length; idx++){
+        DivLists[idx].style.color = "black";
     }
     for(let idx = 0; idx <Alists.length; idx++){
         Alists[idx].style.color = "black";
@@ -46,6 +41,17 @@ function settingDarkWhite(value){
 }
 }
 
+
+
+document.addEventListener("DOMContentLoaded", 
+$('.darkMode').click(e => {
+    // console.log(e.target.hash);
+    settingDarkWhite(!darkSetting);
+    darkSetting = !darkSetting;
+}));
+
+ // 쿠키를 이용하여 흑백모드 적용
+ /*
 const setCookie = function(name, value, day) {
     const date = new Date();
     date.setTime(date.getTime() + day * 60 * 60 * 24 * 1000);
@@ -62,7 +68,7 @@ const changeCookie = function(name, value){
     document.cookie = name + '=' + value + ';';
 }
 
-// deleteCookie("darkMode");
+
     const cookiename = "darkMode";
     // setCookie(cookiename, 'true', 1);
     let cookie = getCookie(cookiename);
@@ -105,4 +111,5 @@ $('#darkMode').click(e => {
         changeCookie(cookiename, 'true');
     }
 }));
+*/
 
